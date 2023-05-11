a8c21e2826708c710ec1796334739371ffa35bd1
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-05-11 10:59:55 +0200

    Basic client setup

---
+++ client/postcss.config.ts
@@ -0,0 +1,8 @@
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
+const config = {
+  plugins: [tailwindcss, autoprefixer],
+};
+
+export default config;
---
+++ client/src/store/store.ts
@@ -0,0 +1,18 @@
+import { AnyAction, ThunkAction, configureStore } from '@reduxjs/toolkit';
+
+import userReducer from './reducers/userReducer';
+
+export const store = configureStore({
+  reducer: {
+   user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AnyAction
+>;
